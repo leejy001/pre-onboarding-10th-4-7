efa04328599a0e548a8230d83c028e60ba99f245
leejy001 <greatness007@naver.com>
2023-05-24 21:19:06 +0900

    refactor: cache 구현

--- src/hooks/useSearchData.ts
+++ src/hooks/useSearchData.ts
@@ -1,11 +1,22 @@
 import { useState } from 'react';
 import { searchTodoList } from '../api/todo';
+import { getCache, setCache } from '../utils/cache';
 
 function useSearchdata() {
   const [isTotal, setIsTotal] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const [isSearchLoading, setIsSearchLoading] = useState(false);
   const [searchListData, setSearchListData] = useState<string[]>([]);
+
+  const getSearchData = async (currentPage: number, inputText: string) => {
+    const cacheData = await getCache(inputText + currentPage);
+    if (!cacheData) {
+      const { data } = await searchTodoList({ q: inputText, page: currentPage, limit: 10 });
+      setCache(inputText + currentPage, data, 60 * 1000);
+      return data;
+    }
+    return cacheData;
+  };
 
   const handleSearchFetch = async (type: string, inputText: string) => {
     if (inputText.trim() === '') {
@@ -19,9 +30,9 @@ function useSearchdata() {
     if (type === 'scroll') setCurrentPage(prev => prev + 1);
     setIsSearchLoading(true);
     const updateCurrentPage = type === 'scroll' ? currentPage + 1 : 1;
-    const { data } = await searchTodoList({ q: inputText, page: updateCurrentPage, limit: 10 });
-    setSearchListData(prev => [...prev, ...data.result]);
-    setIsTotal(data.page * data.limit >= data.total);
+    const searchData = await getSearchData(updateCurrentPage, inputText);
+    setSearchListData(prev => [...prev, ...searchData.result]);
+    setIsTotal(searchData.page * searchData.limit >= searchData.total);
     setIsSearchLoading(false);
   };
   return {
