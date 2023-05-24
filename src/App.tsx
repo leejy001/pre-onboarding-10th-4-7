99a3a6c40211c80bbaf87851c54c3d7971ac4640
leejy001 <greatness007@naver.com>
2023-05-24 19:59:44 +0900

    fix: ReactDOM 오류 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { ThemeProvider } from 'styled-components';
+import Theme from './styles/theme';
+import GlobalStyles from './styles/globalStyle';
+import { TodoProvider } from './context/TodoProvider';
+import { SearchProvider } from './context/SearchProvider';
 import Main from './pages/Main';
 
 const App = () => {
-  return <Main />;
+  return (
+    <TodoProvider>
+      <SearchProvider>
+        <ThemeProvider theme={Theme}>
+          <GlobalStyles />
+          <Main />
+        </ThemeProvider>
+      </SearchProvider>
+    </TodoProvider>
+  );
 };
 
 export default App;
